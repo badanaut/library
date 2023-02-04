bbda1b64b044427a5f5688e0c5ed587d0804e55c
badanaut <49814350+badanaut@users.noreply.github.com>
2023-02-04 10:04:56 +0200

    Fix Delete and Add books functions

--- script.js
+++ script.js
@@ -1,65 +1,97 @@
-const library = document.querySelector('.library');
-const deleteButtons = document.querySelectorAll('.delete-btn');
-
-const myLibrary = [
-  {
-    title: '12 rules for life',
-    author: 'Jordan Peterson',
-    pages: '900',
-    status: 'in progress',
-  },
-];
+// eslint-disable-next-line max-classes-per-file
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const bookStatus = document.querySelector('#status');
+const libraryTable = document.querySelector('.library');
+const addForm = document.querySelector('#add-form');
 
 class Book {
-  constructor(title, author, pages, status) {
+  constructor(
+    title = 'Unknown',
+    author = 'Unknown',
+    pages = 0,
+    isRead = 'Unread',
+  ) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
+    this.isRead = isRead;
   }
 }
 
-function addBookToLibrary(title, author, pages, status) {
-  // do stuff here
-  const book = new Book(title, author, pages, status);
-  myLibrary.push(book);
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook);
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title);
+  }
+
+  getBook(title) {
+    return this.books.find((book) => book.title === title);
+  }
+
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title);
+  }
 }
 
-addBookToLibrary('bla bla', 'tudor', 500, 'finished');
+const library = new Library();
 
-function displayBooks() {
-  myLibrary.forEach((item) => {
-    const Row = document.createElement('tr');
-    const Title = document.createElement('td');
-    const Author = document.createElement('td');
-    const Pages = document.createElement('td');
-    const Status = document.createElement('td');
-    const deleteCell = document.createElement('td');
-    const DeleteBtn = document.createElement('button');
-    Title.textContent = item.title;
-    Author.textContent = item.author;
-    Pages.textContent = item.pages;
-    Status.textContent = item.status;
-    DeleteBtn.textContent = 'Delete';
-    DeleteBtn.classList.add('delete-btn');
-    DeleteBtn.name = item.title;
-    Row.appendChild(Title);
-    Row.appendChild(Author);
-    Row.appendChild(Pages);
-    Row.appendChild(Status);
-    Row.appendChild(deleteCell);
-    deleteCell.appendChild(DeleteBtn);
-    library.appendChild(Row);
-  });
+function displayBooks(books) {
+  while (libraryTable.rows.length > 1) {
+    libraryTable.deleteRow(1);
+  }
+  for (let i = 0; i < books.length; i++) {
+    const bookInfo = document.createElement('tr');
+    const checkbox = books[i].isRead ? 'checked' : 'unchecked';
+    bookInfo.innerHTML = `
+      <td>${books[i].title}</td>
+      <td>${books[i].author}</td>
+      <td>${books[i].pages}</td>
+      <td><input type="checkbox" onclick="changeStatus(this);" ${checkbox}></td>
+      <td><button onclick="removeBook(this);">Remove</button></td>
+    `;
+    libraryTable.appendChild(bookInfo);
+  }
+  addForm.reset();
 }
 
-displayBooks();
-
-deleteButtons.forEach((item) => {
-  item.addEventListener('click', () => {
-    // myLibrary.pop(myLibrary.filter((item) => item === button.name));
-    // console.log(myLibrary);
-    // eslint-disable-next-line no-alert
-    window.alert('succes');
-  });
+addForm.addEventListener('submit', (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  const pages = bookPages.value;
+  const status = bookStatus.checked;
+  const newBook = new Book(title, author, pages, status);
+  library.addBook(newBook);
+  displayBooks(library.books);
 });
+
+function removeBook(book) {
+  library.removeBook(book.parentNode.parentNode.firstElementChild.textContent);
+  book.parentNode.parentNode.remove();
+}
+
+function changeStatus(checkbox) {
+  const title = checkbox.parentNode.parentNode.firstElementChild.textContent;
+  const bookToChange = library.books.find((obj) => obj.title === title);
+  bookToChange.isRead = checkbox.checked;
+}
+
+window.onload = () => {
+  const firstBook = new Book('12 Rules for Life', 'Jordan Peterson', 409, false);
+  const secondBook = new Book('Who Moved My Cheese?', 'Spencer Johnson', 96, true);
+  library.addBook(firstBook);
+  library.addBook(secondBook);
+  displayBooks(library.books);
+};
